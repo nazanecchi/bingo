c9264af311a9e121a11c943dc2cd8d8ef447cf4b
nazita <nazanecchi.cer@gmail.com>
2020-04-18 18:46:57 -0300

    Test columnas ocupadas

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -15,3 +15,18 @@ def test_contar_celdas_ocupadas_menores():
         for celda in fila:
             contador += celda
     assert contador  >=15
+
+def test_columnas_con_contenido():
+    carton1=carton()
+    con=0
+    a=0
+    lista=[0,0,0,0,0,0,0,0,0]
+    for fila in carton1:
+        con=0
+        for celda in fila:
+            lista[con]+=celda
+            con+=1
+    for celda in lista:
+        if celda == 0:
+            a=1
+    assert a==0
