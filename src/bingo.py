ebaa164fe421e95bcf168562b0fb94602361d1a3
nazita <nazanecchi.cer@gmail.com>
2020-05-04 16:47:37 -0300

    agregue funcion mostrar_fila

--- src/bingo.py
+++ src/bingo.py
@@ -5,3 +5,17 @@ def carton():
     (1,0,0,1,1,0,1,0,1),
 )
     return carton
+
+def mostrar_fila(carton, nro_fila):
+    return( carton[nro_fila][0],
+            carton[nro_fila][1],
+            carton[nro_fila][2],
+            carton[nro_fila][3],
+            carton[nro_fila][4],
+            carton[nro_fila][5],
+            carton[nro_fila][6],
+            carton[nro_fila][7],
+            carton[nro_fila][8],
+            )
+
+print (mostrar_fila(carton(), 1))
