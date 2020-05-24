04453b45b0bdb6338a283157adc4dad58fbc9230
nazita <nazanecchi.cer@gmail.com>
2020-05-24 16:55:24 -0300

    cosas q ni idea

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -7,7 +7,8 @@ def test_contar_celdas_ocupadas_mayores():
     contador = 0
     for fila in carton1:
         for celda in fila:
-            contador += celda
+            if(celda>0):
+                contador=contador+1
     assert contador <= 15
 
 def test_contar_celdas_ocupadas_menores():
@@ -15,7 +16,8 @@ def test_contar_celdas_ocupadas_menores():
     contador = 0
     for fila in carton1:
         for celda in fila:
-            contador += celda
+            if(celda>0):
+                contador=contador+1
     assert contador  >=15
 
 def test_columnas_con_contenido():
