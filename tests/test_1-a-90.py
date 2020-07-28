22bd51b8d4c3bf059a42a4310c74db4f77adb8ef
nazaspu <nazanecchi.cer@gmail.com>
2020-07-28 19:06:21 -0300

    agregue el travis 9

--- tests/test_1-a-90.py
+++ tests/test_1-a-90.py
@@ -2,4 +2,4 @@ from src.bingo import carton_oficial
 from src.bingo import uno_a_noventa
 
 def test_uno_a_noventa():
-    assert uno_a_noventa(carton_oficial()) == True
+    assert uno_a_noventa(carton_oficial()) == True 
