363c4ac6615045be3dfe72f989ce340c67d668bc
nazaspu <nazanecchi.cer@gmail.com>
2020-08-24 12:04:51 -0300

    commit

--- tests/test_1-a-90.py
+++ tests/test_1-a-90.py
@@ -1,5 +1,5 @@
 from src.bingo import carton_oficial
 from src.bingo import uno_a_noventa
 
-def test_uno_a_noventa(): 
+def test_uno_a_noventa():
     assert uno_a_noventa(carton_oficial()) == True
